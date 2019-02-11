9c04854d72548b56bfff3be49e8da56ef2158dee
Duminy Gaétan <gaetan.duminy@gmail.com>
2019-02-11 15:59:48 +0100

    Ajout ville dans magasin

--- serverSide/app_modules/storeManager.js
+++ serverSide/app_modules/storeManager.js
@@ -13,9 +13,9 @@ exports.add_store = function (request) {
             { useNewUrlParser: true },
             function (err, client) {
                 var db = client.db(dbName);
-                if (!err && request['nom']!=null && request['adresse']!=null && request['longitude']!=null && request['latitude']!=null) {
+                if (!err && request['nom']!=null && request['adresse']!=null && request['ville']!=null && request['longitude']!=null && request['latitude']!=null) {
                     db.collection("magasin")
-                        .insertOne({nom:request['nom'], adresse:request['adresse'], longitude:request['longitude'], latitude:request['latitude']})
+                        .insertOne({nom:request['nom'], adresse:request['adresse'], ville:request['ville'].toUpperCase(),longitude:request['longitude'], latitude:request['latitude']})
                         .then(x => fun(x));
                 } else {
                     fun(-1);
