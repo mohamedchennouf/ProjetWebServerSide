1607abfc477f45f1eec17e19065fb56d8cf40e6c
Rmeersman <roody06@hotmail.fr>
2019-02-11 15:56:48 +0100

    add post action
    add post action for subsription

--- clientSide/hello-world/src/pages/inscription.js
+++ clientSide/hello-world/src/pages/inscription.js
@@ -10,12 +10,37 @@ class inscription extends Component {
           formulaire:["a","b","c","d","e","f"]
       };
       this.onSubmit = this.onSubmit.bind(this);
-      //this.onChange = this.onChange.bind(this);
     }
 
     onSubmit(){
-        console.log("inscription");
-        console.log(this.state.formulaire[0]);
+        // REQUETES POST
+    // Pour éviter que la page ne se ré-affiche
+        event.preventDefault();
+
+    // Récupération du formulaire. Pas besoin de document.querySelector
+    // ou document.getElementById puisque c'est le formulaire qui a généré
+    // l'événement
+        let form = this.state.formulaire;
+    // Récupération des valeurs des champs du formulaire
+    // en prévision d'un envoi multipart en ajax/fetch
+        let donneesFormulaire = new FormData(this.state.formulaire);
+
+        let url = "/users/subscribe";
+
+        fetch(url, {
+            method: "POST",
+            body: donneesFormulaire
+        })
+        .then(function(responseJSON) {
+            responseJSON.json()
+                .then(function(res) {
+                // Maintenant res est un vrai objet JavaScript
+                    afficheReponsePOST(res);
+                });
+            })
+        .catch(function (err) {
+            console.log(err);
+        });
     }
 
     onChange(e,indice){
@@ -25,7 +50,6 @@ class inscription extends Component {
     }
 
     render() {
-
 
         let renderInputList = this.state.inputList.map(
             (el,index) =>         
