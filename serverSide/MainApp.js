531bffeb3dbd8e505bef3924e7b4359d17482ec8
Duminy Gaétan <gaetan.duminy@gmail.com>
2019-02-11 15:18:20 +0100

    MAJ Score a terminer

--- serverSide/MainApp.js
+++ serverSide/MainApp.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const foodManager = require("./app_modules/foodManager");
 const recetteManager = require("./app_modules/recetteManager");
+const storeManager = require("./app_modules/storeManager");
 const app = express();
 const server = require("http").Server(app);
 const port = process.env.PORT || 8080;
@@ -10,51 +11,65 @@ const bodyParser = require("body-parser");
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/clientSide"));
-const pathClientSide = __dirname.substring(0, __dirname.length - 10)+"clientSide";
+const pathClientSide = __dirname.substring(0, __dirname.length - 10) + "clientSide";
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
   next();
 });
 
 server.listen(port, function () {
-  console.log('Server listening on port '+ port);
+  console.log('Server listening on port ' + port);
 });
 
-app.route("/index").get(function(req, res) {
+app.route("/index").get(function (req, res) {
   res.sendfile("./DebugUI/xxx.html");
 });
 
-app.route("/API/FOODS/RANDOM").get(function(req, res) {
+app.route("/API/FOODS/RANDOM").get(function (req, res) {
   foodManager.getFirstFood().then(x => res.send(x));
 });
 
-app.route("/API/RECETTES").post(function(req, res) {
+app.route("/API/RECETTES").post(function (req, res) {
   var title = req.param("title");
-  var content  = req.param("content");
-  var name  = req.param("name");
+  var content = req.param("content");
+  var name = req.param("name");
   recetteManager.postNouvelleRecette(title, content, name).then(x => res.send(x))
-}).get(function(req, res) {
+}).get(function (req, res) {
   var resu = req.param("res") || 10;
-  var sort  = req.param("sort") || "normal";
-  var page  = eval(req.param("page")) || "0";
-  recetteManager.getRecettes(resu, sort, page).then(x => res.send(x))});
+  var sort = req.param("sort") || "normal";
+  var page = eval(req.param("page")) || "0";
+  recetteManager.getRecettes(resu, sort, page).then(x => res.send(x))
+});
 
-app.route("/API/FOODS").post(function(req, res) {
+app.route("/API/FOODS").post(function (req, res) {
   data = JSON.parse(Object.keys(req.body)[0]);
   console.log(data);
-  foodManager.postFoods(data).then(x => test(x, data, res));
+  foodManager.postFoods(data).then(x => check_results(x, data, res));
 })
 
-function test(x, data, res) {
-  if(x.length == 0) {
-    if(data.hasOwnProperty('nom')) {
+app.route("/API/STORES/ADDTEST").post(function (req, res) {
+  data = JSON.parse(Object.keys(req.body)[0]);
+  console.log(data);
+  storeManager.postFoods(data).then(x => check_results(x, data, res));
+})
+
+app.route("/API/FOODS/MAJSCORE").get(function (req, res) {
+  foodManager.get_all_foods().then(data => {
+    foodManager.maj_custom_score(data);
+    res.send("OK");
+  });
+});
+
+function check_results(x, data, res) {
+  if (x.length == 0) {
+    if (data.hasOwnProperty('nom')) {
       str = JSON.stringify(data);
       str = str.replace(/\"nom\":/g, "\"mot-cle\":");
       json = JSON.parse(str);
@@ -70,23 +85,23 @@ function test(x, data, res) {
 }
 
 //main page
-app.route("/miammiameat").get(function(req, res) {
-  res.sendfile(pathClientSide+"/main.html");
+app.route("/miammiameat").get(function (req, res) {
+  res.sendfile(pathClientSide + "/main.html");
 });
 
 //resources for main page
-app.route("/miammiameat/resources/logo").get(function(req, res) {
-  res.sendfile(pathClientSide+"/resources/logo.png");
+app.route("/miammiameat/resources/logo").get(function (req, res) {
+  res.sendfile(pathClientSide + "/resources/logo.png");
 });
-app.route("/miammiameat/resources/header").get(function(req, res) {
-  res.sendfile(pathClientSide+"/resources/imgHeader.jpg");
+app.route("/miammiameat/resources/header").get(function (req, res) {
+  res.sendfile(pathClientSide + "/resources/imgHeader.jpg");
 });
-app.route("/miammiameat/resources/menu").get(function(req, res) {
-  res.sendfile(pathClientSide+"/resources/menu.jpg");
+app.route("/miammiameat/resources/menu").get(function (req, res) {
+  res.sendfile(pathClientSide + "/resources/menu.jpg");
 });
-app.route("/miammiameat/resources/search").get(function(req, res) {
-  res.sendfile(pathClientSide+"/resources/search.jpg");
+app.route("/miammiameat/resources/search").get(function (req, res) {
+  res.sendfile(pathClientSide + "/resources/search.jpg");
 });
-app.route("/miammiameat/resources/defaultIMG").get(function(req, res) {
-  res.sendfile(pathClientSide+"/resources/defaultIMG.jpg");
+app.route("/miammiameat/resources/defaultIMG").get(function (req, res) {
+  res.sendfile(pathClientSide + "/resources/defaultIMG.jpg");
 });
--- serverSide/app_modules/foodManager.js
+++ serverSide/app_modules/foodManager.js
@@ -6,16 +6,16 @@ const url = "mongodb://Admin:Admin1@ds111455.mlab.com:11455/prod_moxaruga";
 
 const dbName = "prod_moxaruga";
 
-exports.getFirstFood = function() {
+exports.getFirstFood = function () {
   return new Promise(fun => {
     MongoClient.connect(
       url,
-      function(err, client) {
+      function (err, client) {
         var db = client.db(dbName);
         if (!err) {
           db.collection("france")
-          .find({}).limit(10).toArray()
-          .then(x => fun(x));
+            .find({}).limit(10).toArray()
+            .then(x => fun(x));
         } else {
           fun(-1);
         }
@@ -24,20 +24,20 @@ exports.getFirstFood = function() {
   });
 };
 
-exports.postFoods = function(data) {
+exports.postFoods = function (data) {
   var request = JSON.parse(convert(data));
   console.log(request);
   return new Promise(fun => {
     MongoClient.connect(
       url,
       { useNewUrlParser: true },
-      function(err, client) {
+      function (err, client) {
         var db = client.db(dbName);
         if (!err) {
           db.collection("france")
-          .find(request)
-          .toArray()
-          .then(x => fun(x));
+            .find(request)
+            .toArray()
+            .then(x => fun(x));
         } else {
           fun(-1);
         }
@@ -57,9 +57,155 @@ function convert(data) {
     if (i > 0) {
       res += ",";
     }
-    res += "\"" + dict[field] + "\"" +  ":" + "\"" + data[field] + "\"";
+    res += "\"" + dict[field] + "\"" + ":" + "\"" + data[field] + "\"";
     i++;
   }
   res += "}";
   return res;
 };
+
+exports.get_all_foods = function () {
+  return new Promise(fun => {
+    MongoClient.connect(
+      url,
+      function (err, client) {
+        var db = client.db(dbName);
+        if (!err) {
+          db.collection("france")
+            .find({}).toArray()
+            .then(x => fun(x));
+        } else {
+          fun(-1);
+        }
+      }
+    );
+  });
+}
+
+exports.maj_custom_score = function (data) {
+  diff = 0;
+  for (i = 0; i < data.length; i++) {
+    if (data[i]['nutriments']!= null) {
+      diff++;
+      nutriments = data[i]['nutriments'];
+      if (nutriments['sodium_100g']!= null && nutriments['saturated-fat_100g']!= null
+       && nutriments['sugars_100g']!= null && nutriments['energy_100g']!= null && nutriments['proteins_100g']!= null) {
+        diff--;
+        console.log(compute_score(nutriments));
+      }
+    }
+  }
+  console.log("Diff : " + diff);
+  return "";
+}
+
+function compute_score(nutriments) {
+  var score = 0;
+  var sodium = nutriments['sodium_100g'];
+  var saturatedFat = nutriments['saturated-fat_100g'];
+  var sugars = nutriments['sugars_100g'];
+  var energy = nutriments['energy_100g'];
+  var proteins = nutriments['proteins_100g'];
+
+  if (sodium > 0.9) {
+    score += 10;
+  } else if (sodium > 0.81) {
+    score += 9;
+  } else if (sodium > 0.72) {
+    score += 8;
+  } else if (sodium > 0.63) {
+    score += 7;
+  } else if (sodium > 0.54) {
+    score += 6;
+  } else if (sodium > 0.45) {
+    score += 5;
+  } else if (sodium > 0.36) {
+    score += 4;
+  } else if (sodium > 0.27) {
+    score += 3;
+  } else if (sodium > 0.18) {
+    score += 2;
+  } else if (sodium > 0.09) {
+    score += 1;
+  }
+
+  if (saturatedFat > 10) {
+    score += 10;
+  } else if (saturatedFat > 9) {
+    score += 9;
+  } else if (saturatedFat > 8) {
+    score += 8;
+  } else if (saturatedFat > 7) {
+    score += 7;
+  } else if (saturatedFat > 6) {
+    score += 6;
+  } else if (saturatedFat > 5) {
+    score += 5;
+  } else if (saturatedFat > 4) {
+    score += 4;
+  } else if (saturatedFat > 3) {
+    score += 3;
+  } else if (saturatedFat > 2) {
+    score += 2;
+  } else if (saturatedFat > 1) {
+    score += 1;
+  }
+
+  if (sugars > 13.5) {
+    score += 10;
+  } else if (sugars > 12) {
+    score += 9;
+  } else if (sugars > 10.5) {
+    score += 8;
+  } else if (sugars > 9) {
+    score += 7;
+  } else if (sugars > 7.5) {
+    score += 6;
+  } else if (sugars > 6) {
+    score += 5;
+  } else if (sugars > 4.5) {
+    score += 4;
+  } else if (sugars > 3) {
+    score += 3;
+  } else if (sugars > 1.5) {
+    score += 2;
+  } else if (sugars >= 0) {
+    score += 1;
+  }
+
+  if (energy > 3350) {
+    score += 10;
+  } else if (energy > 3015) {
+    score += 9;
+  } else if (energy > 2680) {
+    score += 8;
+  } else if (energy > 2345) {
+    score += 7;
+  } else if (energy > 2010) {
+    score += 6;
+  } else if (energy > 1675) {
+    score += 5;
+  } else if (energy > 1340) {
+    score += 4;
+  } else if (energy > 1005) {
+    score += 3;
+  } else if (energy > 670) {
+    score += 2;
+  } else if (energy > 335) {
+    score += 1;
+  }
+
+  if (proteins > 8) {
+    score -= 10;
+  } else if (proteins > 6.4) {
+    score -= 8;
+  } else if (proteins > 4.8) {
+    score -= 6;
+  } else if (proteins > 3.2) {
+    score -= 4;
+  } else if (proteins > 1.6) {
+    score -= 2;
+  }
+
+  return score;
+}
