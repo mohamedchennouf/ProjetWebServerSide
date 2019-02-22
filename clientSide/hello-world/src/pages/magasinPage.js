6f5e58fe5bcaf4101788174d3e44faa5d72adce8
Rmeersman <roody06@hotmail.fr>
2019-02-22 18:11:32 +0100

    store page update

--- clientSide/hello-world/src/pages/magasinPage.js
+++ clientSide/hello-world/src/pages/magasinPage.js
@@ -3,7 +3,8 @@ import './magasinPage.css';
 import MainFrame from './MainFrame';
 import ReactDOM from 'react-dom';
 import { puts } from 'util';
-/*class magasinJson{
+
+class magasinJson{
     constructor(id,nom,adresse,ville,longitude,latitude){
         this.id = id;
         this.nom = nom;
@@ -12,7 +13,7 @@ import { puts } from 'util';
         this.longitude = longitude;
         this.latitude = latitude;
     }
-}*/
+}
 
 
 class magasinPage extends Component {
@@ -21,44 +22,44 @@ class magasinPage extends Component {
     super(props);
     this.state = {
         cityList : [],
-        magasinList : [],
+        magasinList : {},
       //imgMagasinRecipes: ['resources/couscous.png','resources/pâte.png','resources/unknown.png'],
         stringMagasinText: []
     }
   }
 
-  componentWillMount() {
-    var promise1 = this.cityFetch();
-    Promise.all(promise1)
-    .then(this.storeFetch())
-    .then(console.log("end pre-render"));
+  componentDidMount() {
+    this.cityFetch()
   }
 
   storeFetch(){
-    this.state.cityList.map( (city,index) => {
-      let url = "http://localhost:8080/API/STORES/GET_STORES_CITY";
-      fetch(url,{
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'},
-        body:JSON.stringify({ville : city})
-      })
-      .then(response => response.json())
-      .then(response => response.map((elme) =>{
-        if(this.state.magasinList.length < index+1){
-          this.state.magasinList.push([]);
-        }
-        this.state.magasinList[index].push(elme.nom);
-        console.log("end fetch " + elme.nom)
-      }))
-      .then(console.log("end fetch store"))
-      .catch(function (err) {
-        console.log(err);
-      });
+    let url = "http://localhost:8080/API/STORES/GET_STORES_CITIES";
+    fetch(url,{
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'},
+      body:JSON.stringify({villes : this.state.cityList})
     })
+    .then(response => response.json())
+    //.then(response => console.log(response.store))
+    .then(response => this.constructTabStore(response.store))
+    .catch(function (err) {
+      console.log(err);
+    });
   }
 
+  constructTabStore(data){
+    console.log(data)
+    var tab = {};
+    for(var i = 0 ; i< this.state.cityList.length;i++){
+      //console.log(data.store[0].nom);
+      tab[this.state.cityList[i]] = data[i];
+    }
+    
+   // Promise.all(promesse).then(
+    this.setState({magasinList : tab});
+  }
 
   cityFetch(){
     let url = "http://localhost:8080/API/STORES/GET_CITIES";
@@ -71,8 +72,7 @@ class magasinPage extends Component {
     })
     .then(res => res.json())
     .then(res => this.setState({cityList : res.villes}))
-    .then(console.log("end city fetch"))
-
+    .then(res => this.storeFetch())
     .catch(function (err) {
         console.log(err);
     });
@@ -82,39 +82,44 @@ class magasinPage extends Component {
     return this.state.stringMagasinText[index];
   }
 
-  getName(indice,index){
-    console.log("indice : " + indice + " " + this.state.magasinList[indice]);
-    return this.state.magasinList[indice][index];
+  getName(city,index){
+    return this.state.magasinList[city][index].nom;
 }
 
+  renderHashTable(city){
+    var table = this.state.magasinList[city];
+    console.log(table)
+    table.map(
+      (el, index) => {
+        return <div className="magasinLineBlock">
+          <div className="nomMagasin"> 
+              {this.getName(city,index)}
+          </div>
+          <button className="buttonMap" name={el} indice={index}>
+              Show map
+              <img className="imgButton" alt="" src="resources/maps.png"/>
+          </button>
+          <div className="textMagasin"> 
+              {this.getText(index)}
+          </div>
+      </div>
+  });
+  }
+
   render() {
-    console.log(this.state.magasinList.length)
-    let magasinBlockList = this.state.cityList.map(
-        (city,indice)=>{
+    let magasinBlockList = null;
+    if(Object.keys(this.state.magasinList).length > 0){
+      magasinBlockList = this.state.cityList.map(
+        (city)=>{
             return <div className="cityBlock">
                 <div className="cityNameStyle">
-                    {city}
-                    {console.log(this.state.magasinList.length)}
+                    {city}                
                 </div>
-                {this.state.magasinList.map(
-                    (el, index) => {
-                        return <div className="magasinLineBlock">
-                            <div className="nomMagasin"> 
-                                {this.getName(indice,index)}
-                            </div>
-                            <button className="buttonMap" name={el} indice={index}>
-                                Show map
-                                <img className="imgButton" alt="" src="resources/maps.png"/>
-                            </button>
-                            <div className="textMagasin"> 
-                                {this.getText(index)}
-                            </div>
-                        </div>
-                    }
-                )}
+                {this.renderHashTable(city)}
             </div>
         }
-    );
+      );
+      }
     let insideContent = 
     <div className="body-content">
     <div>
