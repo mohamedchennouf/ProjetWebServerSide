7204b0a26ebb9aa456f73b04dfdfa3f9aa449fa8
MEERSMAN Rudy <roody06@hotmail.fr>
2019-02-24 15:33:41 +0100

    Update advancedSearchResult.js
    jpp

--- clientSide/hello-world/src/pages/advancedSearchResult.js
+++ clientSide/hello-world/src/pages/advancedSearchResult.js
@@ -1,34 +1,59 @@
 import React, { Component } from "react";
 import "./advancedSearchResult.css";
 import MainFrame from "./MainFrame";
-import settings from './../settings';
+import { cpus } from "os";
 
 class advancedSearchResult extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        advancedArgs : ["", "", "", "", ""],
+        advancedArgs : [],
         keyWord :[]
       };
     }
 
-    componentWillMount() {
-      this.setState({advancedArgs : localStorage.getItem("advancedR")});
-      this.setState({keyWord : localStorage.getItem("keyWord")});
+    takeCookie(){
+      return new Promise((x) => {
+      var arg = localStorage.getItem("advancedR").split(",");
+      var key = localStorage.getItem("keyWord").split(",");
+      console.log(arg);
+      this.setState({advancedArgs : arg, keyWord : key});
+      console.log(this);
+      console.log(this.state.advancedArgs);
       localStorage.clear();
+<<<<<<< HEAD
+      x();
+      });
+    }
+
+
+    componentDidMount() {
+      if(localStorage.getItem("advancedR") != null){
+        this.takeCookie()
+       .then(console.log(this.state.advancedArgs[0]))
+       // .then(this.searchFetch());
+      }
+=======
       this.searchFetch();
+      
+>>>>>>> 731960e033f7466c62d8ec0bf9e57b558c7a1f58
     }
 
     searchFetch() {
-        var args = this.state.advancedArgs
-        fetch(settings.url + "API/ADVANCE_SEARCH", {
+        let url = "http://localhost:8080/API/FOODS/ADVANCE_SEARCH";
+        var args = this.state.advancedArgs;
+        console.log("{nom: "+ args[0]+ ", ville : " + args[1] + ", magasin : " + args[2] + ", prix : " +args[3] + ",Comparateur: <=, marque: " + args[4] + ", motCle:" + this.state.keyWord + "}");
+          //    Comparateur: "<=", marque: args[4], motCle: this.state.keyWord})
+        fetch(url , {
           method: "POST",
           headers: {
-            Accept: "application/json",
+              Accept: "application/json",
             "Content-Type": "application/json"
+            
           },
-          body: JSON.stringify({nom: args[0],ville : args[1],magasin : args[2], prix :args[3],
-             Comparateur: "<=", marque: args[4], motCle: this.state.keyWord})
+          body: JSON.stringify({nom: args[0]})
+        //  body: JSON.stringify({nom: args[0], ville : args[1], magasin : args[2], prix :args[3],
+         //    Comparateur: "<=", marque: args[4], motCle: this.state.keyWord})
         })
           .then(response => response.json())
           .then(response => console.log(response))
